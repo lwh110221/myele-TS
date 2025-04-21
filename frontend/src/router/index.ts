6ad1af865f1d6695189a0031dfc895116ebcce99
lwh110221 <2285813721@outlook.com>
2025-04-22 00:05:35 +0800

    fix

--- frontend/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import routerMap from './routerMap'
-
-const Router = createRouter({
-  history: createWebHashHistory(),
-  routes: routerMap as RouteRecordRaw[],
-})
-
-export default Router
